0603c2fa818e6b012182ea491fe4485428f9ff61
AniketKaushik19 <aniketsskaushik@gmail.com>
2025-07-12 21:46:04 +0530

    user profile update

--- frontend/src/components/FriendCard.jsx
+++ frontend/src/components/FriendCard.jsx
@@ -5,11 +5,10 @@ const FriendCard = ({friend}) => {
   return (
    <div className='card bg-base-200 hover:shadow-md transition-shadow'>
      <div className='card-body p-4 '>
-
         {/* User Info  */}
         <div className='flex items-center gap-3 mb-3'>
-            <div className='avatar siz-6'>
-                <img src={friend.profilepic} alt={friend.fullname} />
+            <div className='avatar w-16'>
+                <img src={friend.profilepic} alt={friend.fullname}/>
             </div>
 
             <h3 className='font-semibold truncate'>{friend.fullname}</h3>
